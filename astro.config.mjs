fee69d5df09399b916239f988a5538dd342bee0a
Mariolodev <mariolodehf@gmail.com>
2025-06-15 17:33:10 -0500

    Fix: correcciones en npm

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,45 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://puntozero.dev',
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    sitemap()
+  ],
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    },
+    imageService: true,
+    imagesConfig: {
+      sizes: [640, 750, 828, 1080, 1200, 1920, 2048],
+      domains: ['puntozero.dev'],
+      minimumCacheTTL: 60
+    }
+  }),
   markdown: {
     shikiConfig: {
       theme: 'github-light',
       wrap: true
+    }
+  },
+  vite: {
+    build: {
+      cssMinify: true,
+      minify: true,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            'astro': ['astro'],
+            'tailwind': ['tailwindcss']
+          }
+        }
+      }
     }
   }
 });
